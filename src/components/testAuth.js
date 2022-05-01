c8b20c43dd4d2704bd534e79b852b7655849b9df
aliwtw <alihwtw@gmail.com>
2022-05-01 11:23:57 +1000

    change before merge

--- src/components/testAuth.js
+++ src/components/testAuth.js
@@ -1,6 +1,6 @@
 import config from './firebaseAuth';
 import {initializeApp} from 'firebase/app';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from 'react';
 
 function Auth (){
@@ -8,6 +8,8 @@ function Auth (){
 
     const app = initializeApp(config);
     const auth = getAuth(app);
+
+
     function newUser(email, password){
         createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
@@ -26,10 +28,29 @@ function Auth (){
     }
     
 
+    function signIn(email, password){
+
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            console.log(user);
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            setError(error.message);
+            console.log(errorCode);
+            console.log(errorMessage);
+        });
+    }
+
 
     return(
         <div>
-            <h1 onClick={()=>{newUser('ali@uow.com','123')}}>Test Sign up</h1>
+            <h1 onClick={()=>{newUser('yuin321@gmail.com','123456')}}>Test Sign up</h1>
+            <h1 onClick={()=>{signIn('ali@uow.com','123456')}}>Test Sign in</h1>
+            <br/>
             <h1>{errorMessage}</h1>
         </div>
     )
