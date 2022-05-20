43ec7044a0f8680d47985612ae06fa7364cc2c0f
MCha1128 <103953026+MCha1128@users.noreply.github.com>
2022-05-20 14:21:50 +1000

    Add files via upload

--- src/graphics/index.js
+++ src/graphics/index.js
@@ -1,7 +1,8 @@
 const IMAGES = {
   userIcon: require('./user_logo.png'),
   mechanicIcon: require('./mechanic_logo.png'),
-  onDemandIcon: require('./on_demand_logo.png')
+  onDemandIcon: require('./on_demand_logo.png'),
+  tickIcon: require('./tickImage.png')
 }
 
 export default IMAGES;
