68516b84be086bc52065eb4753e4e35f70d3d309
Xe Yuin Chong <cxy856@uowmail.edu.au>
2022-05-24 15:37:36 +1000

    before pull

--- src/components/testAuth.js
+++ src/components/testAuth.js
@@ -1,6 +1,6 @@
 import config from './firebaseAuth';
 import {initializeApp} from 'firebase/app';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { useState } from 'react';
 
 function Auth (){
@@ -8,15 +8,12 @@ function Auth (){
 
     const app = initializeApp(config);
     const auth = getAuth(app);
-
-
     function newUser(email, password){
         createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             // Signed in 
             const user = userCredential.user;
             console.log(user);
-            setError("New user created");
             // ...
         })
         .catch((error) => {
@@ -29,30 +26,9 @@ function Auth (){
     }
     
 
-    function signIn(email, password){
-
-        signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed in 
-            const user = userCredential.user;
-            console.log(user);
-            setError("User Logged in");
-            // ...
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            setError(error.message);
-            console.log(errorCode);
-            console.log(errorMessage);
-        });
-    }
-
-
     return(
         <div>
-            <h1 onClick={()=>{newUser('ali@uow.com','123456')}}>Test Sign up</h1>
-            <h1 onClick={()=>{signIn('ali@uow.com','123456')}}>Test Sign in</h1>
-            <br/>
+            <h1 onClick={()=>{newUser('ali@uow.com','123')}}>Test Sign up</h1>
             <h1>{errorMessage}</h1>
         </div>
     )
