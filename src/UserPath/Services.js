23b66c766542441712865d61af5675fac94ea9e0
Xe Yuin Chong <cxy856@uowmail.edu.au>
2022-05-29 01:41:49 +1000

    Service Form

--- src/UserPath/Services.js
+++ src/UserPath/Services.js
@@ -1,58 +1,76 @@
 import React, { useState } from "react";
 import './Services.css';
 import Banner from "../pages/banner";
-import IMAGES from "../graphics";
 import { Link } from "react-router-dom";
+import { Form, Button, Card } from "react-bootstrap";
 
-const Services = () => {
+const RequestService = () => {
   
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()  
+    setLoading(true) 
+    const form = event.target.elements 
+    console.log(form)
+  }
+
+
   return (
-    <div className="service_page">
-      <Banner title="How can we help?"/>
-      
-      <div className='service_page_option'>
-        <Link to="/user/services/tow">
-          <button className="service_page_button">Tow</button>
-        </Link> 
-        <span>$100</span>
-      </div>
-      
-      <div className='service_page_option'>
-        <Link to="/user/services/tow">
-          <button className="service_page_button">Lockout</button>
+    <div className="container">
+        <Banner title="How can we help?"/>
+        <Card style={{ margin: '0' }}>
+          <Card.Body>
+            <h2 className="text-center mb-4">Service Request</h2>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group id="vehicleMake">
+                <Form.Label>Vehicle Make</Form.Label>
+                <Form.Control type="text" required />
+              </Form.Group>
+              <Form.Group id="vehicleModel">
+                <Form.Label>Vehicle Model</Form.Label>
+                <Form.Control type="text" required />
+              </Form.Group>
+              <Form.Label>Service Required</Form.Label>
+              <Form.Group>
+                {['checkbox'].map((type) => (
+                  <div key={`default-${type}`} className="mb-3">
+                    <Form.Check 
+                      type={type}
+                      id={`towing`}
+                      label={`Towing`}
+                    />
+                    <Form.Check 
+                      type={type}
+                      id={`fuel`}
+                      label={`Fuel Delivery`}
+                    />
+                    <Form.Check 
+                      type={type}
+                      id={`battery`}
+                      label={`Battery Replacement`}
+                    />
+                    
+                  </div>
+                ))}
+              </Form.Group>
+              <Form.Group id="description">
+                <Form.Label>Brief Description</Form.Label>
+                <Form.Control type="textarea" required />
+              </Form.Group>
+              <Button disabled={loading} className="w-100 mt-3" type="submit">
+                Submit Request
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
+        <Link to='/user' style={{marginTop: '1rem'}}>
+          <span>
+            Go Back
+          </span>
         </Link>
-        <span>$79</span>
       </div>
-
-      <div className='service_page_option'>
-        <Link to="/user/services/tow">
-          <button className="service_page_button">Fuel Delivery</button>
-        </Link>
-        <span>$79</span>
-      </div>
-
-      <div className='service_page_option'>
-        <Link to="/user/services/tow">
-          <button className="service_page_button">Tire Change</button>
-        </Link>
-        <span>$85</span>
-      </div>
-
-      <div className='service_page_option'>
-        <Link to="/user/services/tow">
-          <button className="service_page_button">Jump Start</button>
-        </Link>
-        <span>$80</span>
-      </div>
-
-      <Link to='/user' style={{marginTop: '1rem'}}>
-        <span>
-          Go Back
-        </span>
-      </Link>
-      
-    </div>
   );
 }
 
-export default Services;
+export default RequestService;
